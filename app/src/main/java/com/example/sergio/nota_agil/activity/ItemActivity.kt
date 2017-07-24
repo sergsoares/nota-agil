132fdc35ebf89accf7bd1c7b298c4a2520716031
Sergio Soares <acad.sergio@gmail.com>
2017-07-24 18:17:26 -0400

    Define Constant for Item and mock Add Function

--- app/src/main/java/com/example/sergio/nota_agil/activity/ItemActivity.kt
+++ app/src/main/java/com/example/sergio/nota_agil/activity/ItemActivity.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import org.jetbrains.anko.listView
-import org.jetbrains.anko.onItemClick
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
-import java.util.*
+import io.paperdb.Paper
+import org.jetbrains.anko.*
 
 /**
  * Created by sergio on 24/07/17.
@@ -17,8 +14,13 @@ import java.util.*
 
 class ItemActivity : AppCompatActivity() {
 
+  lateinit var CATEGORY: String
+  lateinit var ITEM: String
+
   public override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    CATEGORY = intent.getStringExtra("category")
+    ITEM = intent.getStringExtra("item")
     defineLayout()
     reloadAdapter()
   }
@@ -27,8 +29,18 @@ class ItemActivity : AppCompatActivity() {
 
   private fun defineLayout() {
     verticalLayout {
+      button {
+        text = "Gravar Audio"
+        onClick {
+          val itens = if (item == null) ArrayList<String>() else item!!
+          itens.add("/123.3gp")
+          itens.add("/321.3gp")
+          Paper.book(CATEGORY).write(ITEM, itens)
+          reloadAdapter()
+        }
+      }
       textView {
-        text = intent.getSerializableExtra("item").toString()
+        text = ITEM
         textSize = 42f
       }
       filesListView = listView {
@@ -39,8 +51,11 @@ class ItemActivity : AppCompatActivity() {
     }
   }
 
+  private var item: ArrayList<String>? = null
+
   private fun reloadAdapter() {
-      val item = intent.getSerializableExtra("item") as ArrayList<String>
+      item = Paper.book(CATEGORY).read(ITEM)
+//      item = intent.getSerializableExtra("item") as ArrayList<String>
       val adapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, item)
       filesListView!!.adapter = adapter
   }
