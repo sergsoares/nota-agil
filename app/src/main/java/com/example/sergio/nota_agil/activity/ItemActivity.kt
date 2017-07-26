34377c089147d5f431dceb61738b354c32d8f26c
Sergio Soares <acad.sergio@gmail.com>
2017-07-25 21:28:21 -0400

    Removed Lombok and Configured Google Drive button for test

--- app/src/main/java/com/example/sergio/nota_agil/activity/ItemActivity.kt
+++ app/src/main/java/com/example/sergio/nota_agil/activity/ItemActivity.kt
@@ -2,7 +2,11 @@ package com.example.sergio.nota_agil.activity
 
 import android.Manifest
 import android.R
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
@@ -16,24 +20,27 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.drive.Drive
+import com.google.android.gms.drive.DriveApi
+import com.google.android.gms.drive.MetadataChangeSet
 import io.paperdb.Paper
 import org.jetbrains.anko.*
+import java.io.ByteArrayOutputStream
 import java.io.IOException
 
-class ItemActivity : AppCompatActivity() {
+class ItemActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
   private var CATEGORY: String = ""
   private var ITEM: String = ""
-  //  lateinit var ITEM: String
   private val TAG = "ItemActivity"
   private var filesListView: ListView? = null
-//  private var mMediaPlayer: MediaPlayer? = null
   private val mFileName: StringBuilder = StringBuilder()
-
-//  companion object {
-    private var mMediaRecorder: MediaRecorder? = null
-    private var mMediaPlayer: MediaPlayer? = null
-//  }
+  private var mMediaRecorder: MediaRecorder? = null
+  private var mMediaPlayer: MediaPlayer? = null
 
 
   public override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,6 +77,11 @@ class ItemActivity : AppCompatActivity() {
           recordButton?.visibility = View.VISIBLE
           stopRecordButton?.visibility = View.GONE
         }
+      }
+
+      button {
+        text = "Test Google Drive"
+        onClick { startActivity<GoogleDriveActivity>() }
       }
 
       textView {
@@ -193,5 +205,141 @@ class ItemActivity : AppCompatActivity() {
     if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
       Log.v(TAG, "Permission: " + permissions[0] + " was " + grantResults[0])
     }
+  }
+
+  override fun onConnectionSuspended(cause: Int) {
+    Log.i(ItemActivity.TAG, "GoogleApiClient connection suspended")
+  }
+
+  private var mBitmapToSave: Bitmap? = null
+
+  override fun onConnected(connectionHint: Bundle?) {
+    Log.i(ItemActivity.TAG, "API client connected.")
+//    if (mBitmapToSave == null) {
+//      // This activity has no UI of its own. Just start the camera.
+//      startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE),
+//          ItemActivity.REQUEST_CODE_CAPTURE_IMAGE)
+//      return
+//    }
+//    saveFileToDrive()
+  }
+
+  private var mGoogleApiClient: GoogleApiClient? = null
+
+  private fun saveFileToDrive() {
+    // Start by creating a new contents, and setting a callback.
+    Log.i(ItemActivity.TAG, "Creating new contents.")
+    val image = mBitmapToSave
+    Drive.DriveApi.newDriveContents(mGoogleApiClient)
+        .setResultCallback(ResultCallback<DriveApi.DriveContentsResult> { result ->
+          // If the operation was not successful, we cannot do anything
+          // and must
+          // fail.
+          if (!result.status.isSuccess) {
+            Log.i(ItemActivity.TAG, "Failed to create new contents.")
+            return@ResultCallback
+          }
+          // Otherwise, we can write our data to the new contents.
+          Log.i(ItemActivity.TAG, "New contents created.")
+          // Get an output stream for the contents.
+          val outputStream = result.driveContents.outputStream
+          // Write the bitmap data from it.
+          val bitmapStream = ByteArrayOutputStream()
+          image!!.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream)
+          try {
+            outputStream.write(bitmapStream.toByteArray())
+          } catch (e1: IOException) {
+            Log.i(ItemActivity.TAG, "Unable to write file contents.")
+          }
+
+          // Create the initial metadata - MIME type and title.
+          // Note that the user will be able to change the title later.
+          val metadataChangeSet = MetadataChangeSet.Builder()
+              .setMimeType("image/jpeg").setTitle("Android Photo.png").build()
+          // Create an intent for the file chooser, and start it.
+          val intentSender = Drive.DriveApi
+              .newCreateFileActivityBuilder()
+              .setInitialMetadata(metadataChangeSet)
+              .setInitialDriveContents(result.driveContents)
+              .build(mGoogleApiClient!!)
+          try {
+            startIntentSenderForResult(
+                intentSender, ItemActivity.REQUEST_CODE_CREATOR, null, 0, 0, 0)
+          } catch (e: IntentSender.SendIntentException) {
+            Log.i(ItemActivity.TAG, "Failed to launch file chooser.")
+          }
+        })
+  }
+
+  override fun onResume() {
+    super.onResume()
+    if (mGoogleApiClient == null) {
+      // Create the API client and bind it to an instance variable.
+      // We use this instance as the callback for connection and connection
+      // failures.
+      // Since no account name is passed, the user is prompted to choose.
+      mGoogleApiClient = GoogleApiClient.Builder(this)
+          .addApi(Drive.API)
+          .addScope(Drive.SCOPE_FILE)
+          .addConnectionCallbacks(this)
+          .addOnConnectionFailedListener(this)
+          .build()
+    }
+    // Connect the client. Once connected, the camera is launched.
+    mGoogleApiClient!!.connect()
+  }
+
+  override fun onPause() {
+    if (mGoogleApiClient != null) {
+      mGoogleApiClient!!.disconnect()
+    }
+    super.onPause()
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    when (requestCode) {
+      ItemActivity.REQUEST_CODE_CAPTURE_IMAGE ->
+        // Called after a photo has been taken.
+        if (resultCode == Activity.RESULT_OK) {
+          // Store the image data as a bitmap for writing later.
+          mBitmapToSave = data.extras.get("data") as Bitmap
+        }
+      ItemActivity.REQUEST_CODE_CREATOR ->
+        // Called after a file is saved to Drive.
+        if (resultCode == Activity.RESULT_OK) {
+          Log.i(ItemActivity.TAG, "Image successfully saved.")
+          mBitmapToSave = null
+          // Just start the camera again for another photo.
+//          startActivityForResult(Intent(MediaStore.ACTIONIMAGE_CAPTURE),
+//              ItemActivity.REQUEST_CODE_CAPTURE_IMAGE)
+        }
+    }
+  }
+
+  override fun onConnectionFailed(result: ConnectionResult) {
+    // Called whenever the API client fails to connect.
+    Log.i(ItemActivity.TAG, "GoogleApiClient connection failed: " + result.toString())
+    if (!result.hasResolution()) {
+      // show the localized error dialog.
+      GoogleApiAvailability.getInstance().getErrorDialog(this, result.errorCode, 0).show()
+      return
+    }
+    // The failure has a resolution. Resolve it.
+    // Called typically when the app is not yet authorized, and an
+    // authorization
+    // dialog is displayed to the user.
+    try {
+      result.startResolutionForResult(this, ItemActivity.REQUEST_CODE_RESOLUTION)
+    } catch (e: IntentSender.SendIntentException) {
+      Log.e(ItemActivity.TAG, "Exception while starting resolution activity", e)
+    }
+
+  }
+
+  companion object {
+    private val TAG = "drive-quickstart"
+    private val REQUEST_CODE_CAPTURE_IMAGE = 1
+    private val REQUEST_CODE_CREATOR = 2
+    private val REQUEST_CODE_RESOLUTION = 3
   }
 }
