1a19f85d9b1605a3007d9788d2da0fed8ae1a1a6
Sergio Soares <acad.sergio@gmail.com>
2017-07-25 21:36:26 -0400

    Cleaned CategoriesActivity

--- app/src/main/java/com/example/sergio/nota_agil/activity/CategoriesActivity.kt
+++ app/src/main/java/com/example/sergio/nota_agil/activity/CategoriesActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.ListAdapter
 import android.widget.ListView
 import io.paperdb.Paper
 import org.jetbrains.anko.*
@@ -18,8 +17,6 @@ class CategoriesActivity : AppCompatActivity() {
   companion object {
     private val TAG = "CategoriesActivity"
     private var allCategories: MutableList<String>? = null
-//    private var layout: LinearLayout? = null
-//    private var adapterCategories: ArrayAdapter<String>? = null
     private var  categoriesListView: ListView? = null
   }
 
@@ -42,7 +39,6 @@ class CategoriesActivity : AppCompatActivity() {
 
       categoriesListView = listView {
         onItemClick { adapterView, view, i, l ->
-//          toast(Paper.book().allKeys[i])
           startActivity<ItemsActivity>("category" to Paper.book().allKeys[i])
         }
       }
@@ -50,16 +46,10 @@ class CategoriesActivity : AppCompatActivity() {
   }
 
   private fun addedInitialContent() {
-    //Criei uma Lista que vai conter meus audios
     val itens = ArrayList<String>()
     itens.add("/123.3gp")
     itens.add("/321.3gp")
 
-    //Criar minha categoria que vai ser representada por Hash
-//    val categories = HashMap<String, List<String>>()
-
-    //Vou anexar nessa categoria uma lista de itens com o nome a minha categoria
-    //Vou adicionar ao book minha categoria com seu nome (key)
     Paper.book("programacao").write("Orientada a objetos", itens)
     Paper.book("programacao").write("SOLID", itens)
     Paper.book("programacao").write("Teste", itens)
@@ -68,7 +58,7 @@ class CategoriesActivity : AppCompatActivity() {
 
   private fun reloadAdapter(){
     val adapterCategories = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Paper.book().allKeys)
-    categoriesListView!!.adapter = adapterCategories as ListAdapter?
+    categoriesListView!!.adapter = adapterCategories
   }
 
 
@@ -82,7 +72,6 @@ class CategoriesActivity : AppCompatActivity() {
 
           //book default save others book Names
           Paper.book().write(input.text.toString(), input.text.toString())
-//          Paper.book("programacao").write(input.text.toString(), ArrayList<String>())
           reloadAdapter()
         }.show();
   }
